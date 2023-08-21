429aef7c1ecc4a1096aaf559cfaff69a7f457492
Galez <89478647+Thapakornd@users.noreply.github.com>
2023-08-21 14:06:27 +0700

    Delete old templete to create new one

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import Content from "./components/Content/Content";
-import Footer from "./components/Footer/Footer";
-import Experience from "./components/Experience/Experience";
-import Skillset from "./components/Skillset/Skillset";
 
 function App() {
   return (
     <>
       <div className="app">
-        <Navbar />
-        <Hero />
-        <Experience />
-        <Skillset />
-        <Content />
-        <Footer />
+
       </div>
     </>
   );
