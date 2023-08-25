b5671023576df79825f466902cc1553838bb703d
Galez <89478647+Thapakornd@users.noreply.github.com>
2023-08-25 23:51:05 +0700

    Create navbar and hero page responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import './Navbar.scss';
+import React, { useEffect, useState } from "react";
+import ViewStreamIcon from "@mui/icons-material/ViewStream";
+import "./Navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-        <div className="logo">
-            <h1>Thapakorn.<span>GUS</span></h1>
+    <div className="navbar">
+      <div className="logo">
+        <h1>
+          Thapakorn.<span>GUS</span>
+        </h1>
+        <div className="check-btn">
+          <ViewStreamIcon sx={{ fontSize: "2.5rem"}}/>
         </div>
-        <ul>
-            <li><a href="#about">ABOUT</a></li>
-            <li><a href="#projects">PROJECTS</a></li>
-            <li className='contact'><a href="#contact">CONTACT</a></li>
-        </ul>
+      </div>
+      <ul>
+        <li>
+          <a href="#about">ABOUT</a>
+        </li>
+        <li>
+          <a href="#projects">PROJECTS</a>
+        </li>
+        <li className="contact">
+          <a href="#contact">CONTACT</a>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
